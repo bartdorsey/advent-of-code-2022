c666b7447a3e240f2ec155c870cd7e7b21fd2f81
Bart Dorsey <bart@bartdorsey.com>
2022-12-06 12:36:31 -0600

    Day 6 functional style

--- src/day6.rs
+++ src/day6.rs
@@ -1,39 +1,33 @@
-use anyhow::{anyhow, Result};
-use std::collections::HashSet;
+use anyhow::Result;
+use std::collections::BTreeSet;
 use std::fs;
 
 pub fn part1() -> Result<u32> {
+    const WINDOW_SIZE: usize = 4;
     let contents = fs::read_to_string("data/day6-input.txt")?;
-    let mut last_four: Vec<char> = vec![];
-    for (index, char) in contents.chars().enumerate() {
-        last_four.push(char);
-        if last_four.len() == 4 {
-            let set: HashSet<&char> = HashSet::from_iter(last_four.iter());
-            if set.len() == 4 {
-                return Ok((index + 1) as u32);
-            } else {
-                last_four.remove(0);
-            }
-        }
-    }
-
-    Err(anyhow!("Couldn't find it"))
+    let chars: Vec<char> = contents.chars().collect();
+    let (count, _) = chars
+        .windows(WINDOW_SIZE)
+        .enumerate()
+        .find(|(_i, window)| {
+            let set = window.iter().collect::<BTreeSet<&char>>();
+            set.len() == WINDOW_SIZE
+        })
+        .unwrap();
+    Ok((count + WINDOW_SIZE) as u32)
 }
 
 pub fn part2() -> Result<u32> {
+    const WINDOW_SIZE: usize = 14;
     let contents = fs::read_to_string("data/day6-input.txt")?;
-    let mut last_four: Vec<char> = vec![];
-    for (index, char) in contents.chars().enumerate() {
-        last_four.push(char);
-        if last_four.len() == 14 {
-            let set: HashSet<&char> = HashSet::from_iter(last_four.iter());
-            if set.len() == 14 {
-                return Ok((index + 1) as u32);
-            } else {
-                last_four.remove(0);
-            }
-        }
-    }
-
-    Err(anyhow!("Couldn't find it"))
+    let chars: Vec<char> = contents.chars().collect();
+    let (count, _) = chars
+        .windows(WINDOW_SIZE)
+        .enumerate()
+        .find(|(_i, window)| {
+            let set = window.iter().collect::<BTreeSet<&char>>();
+            set.len() == WINDOW_SIZE
+        })
+        .unwrap();
+    Ok((count + WINDOW_SIZE) as u32)
 }
